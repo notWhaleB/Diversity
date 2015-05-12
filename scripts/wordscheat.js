9c71b61735317b6fdaccc17e26ac328af5fc4506
Nikita <o0oflashero0o@gmail.com>
2015-05-12 18:46:14 +0300

    Words cheat added.

---
+++ scripts/wordscheat.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+});
+
+function foo() {
+    var string = $("#wordscheat-input").val();
+    $.getJSON("http://serv.begishev.me", string)
+        .done(function(json) {
+            var result_html = "";
+            for (var i = 0; i != json.data.length; ++i) {
+               result_html += json.data[i] + "<br>";
+            }
+            $("#wordscheat-results").append(result_html);
+        });
+}
